4885b35a77fbb3aa58ac60ad2d89bb4383c5bac3
Omochice <mochice.mls.ntl@gmail.com>
2025-06-09 01:26:32 +0900

    fixup! fixup! fixup! ci: composite action

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,37 @@
-import { debug, getInput, info, setOutput } from "npm:@actions/core@1.10.1";
+import { debug, error, info, setOutput } from "npm:@actions/core@1.10.1";
 import { context } from "npm:@actions/github@6.0.0";
 import { normalize, trimTailDotVim } from "./normalize.ts";
+import { parseArgs } from "jsr:@std/cli@1.0.19/parse-args";
 
-const main = () => {
+const main = (args: string[]) => {
   info("Start main process");
-  debug(JSON.stringify(context.repo));
-  const stripDotVim =
-    getInput("trim-tail-dot-vim", { required: false }) === "true";
+  const a = parseArgs(args, {
+    boolean: ["trim-tail-dot-vim"],
+  });
+  const target = a._.at(0);
+  if (target == null || typeof target !== "string") {
+    error(
+      `You must specify a target repository name as the first argument. Received: ${
+        JSON.stringify(
+          target,
+        )
+      }`,
+    );
+    Deno.exit(1);
+  }
+  debug(target);
+  // const stripDotVim =
+  //   getInput("trim-tail-dot-vim", { required: false }) === "true";
+  const stripDotVim = a["trim-tail-dot-vim"];
   if (stripDotVim) {
     info(`You specify trim-tail-dot-vim.`);
   }
-  info("Complete main process");
   const normalized = stripDotVim
     ? trimTailDotVim(normalize(context.repo.repo))
     : normalize(context.repo.repo);
+  info("Complete main process");
   debug(`normalized is "${normalized}"`);
   setOutput("normalizedName", normalized);
 };
 
-main();
+main(Deno.args);
